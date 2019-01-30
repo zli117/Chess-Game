185c77c79f35f66e737098337dfe265c821bc8a6
Zonlgin-Li6565 <development.my6565@gmail.com>
2019-01-29 23:49:48 -0600

    Added Queen

---
+++ src/main/java/model/Queen.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import utils.Direction;
+
+public class Queen extends UnboundedPiece {
+
+  public Queen(ChessBoardBase chessBoard, Side side) {
+    super(chessBoard, side);
+  }
+
+  @Override
+  protected ArrayList<Direction> getDirections() {
+    Direction[] directions = {
+        new Direction(1, 1),
+        new Direction(-1, -1),
+        new Direction(-1, 1),
+        new Direction(1, -1),
+        new Direction(1, 0),
+        new Direction(-1, 0),
+        new Direction(0, 1),
+        new Direction(0, -1)};
+    return new ArrayList<>(Arrays.asList(directions));
+  }
+}
