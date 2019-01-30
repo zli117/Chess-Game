ca8d67b81c8e85521ef121114ac1c87e9f3b7ac0
Zonlgin-Li6565 <development.my6565@gmail.com>
2019-01-29 23:40:10 -0600

    Added Piece base class, and UnboundedPiece base class

---
+++ src/main/java/model/Piece.java
@@ -0,0 +1,68 @@
+package model;
+
+import java.util.Set;
+import utils.Location;
+import utils.Move;
+
+
+public abstract class Piece {
+
+  private ChessBoardBase mChessBoard;
+  private Location mLocation;
+  private Side mSide;
+
+  /**
+   * Create a chess piece
+   *
+   * @param chessBoard A reference to the chess board
+   * @param side The side the piece belongs to
+   */
+  Piece(ChessBoardBase chessBoard, Side side) {
+    mChessBoard = chessBoard;
+    mSide = side;
+  }
+
+  /**
+   * Get the current location
+   */
+  public Location getLocation() {
+    return mLocation;
+  }
+
+  /**
+   * Called when the piece has been moved
+   *
+   * @param location The new location. Will be null if the piece has been
+   * captured
+   */
+  public void setLocation(Location location) {
+    mLocation = location;
+  }
+
+  /**
+   * Get get chess board it belongs to
+   */
+  ChessBoardBase getChessBoard() {
+    return mChessBoard;
+  }
+
+  /**
+   * Get which side this piece belongs to
+   */
+  public Side getSide() {
+    return mSide;
+  }
+
+  /**
+   * Get a set of possible moves and attacks
+   */
+  public abstract Set<Move> getMovesAndAttacks();
+
+  /**
+   * Get string representation
+   */
+  public String toString() {
+    return this.getClass().getName();
+  }
+
+}
---
+++ src/main/java/model/UnboundedPiece.java
@@ -0,0 +1,56 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import utils.Direction;
+import utils.Location;
+import utils.Move;
+
+/**
+ * Unbounded pieces are those can move along a line. Examples are Queen, Rook
+ * etc.
+ */
+public abstract class UnboundedPiece extends Piece {
+
+  UnboundedPiece(ChessBoardBase chessBoard, Side side) {
+    super(chessBoard, side);
+  }
+
+  /**
+   * Return directions to move in a straight line
+   */
+  protected abstract ArrayList<Direction> getDirections();
+
+  @Override
+  public Set<Move> getMovesAndAttacks() {
+    LinkedHashSet<Move> allMoves = new LinkedHashSet<>();
+    ArrayList<Direction> directions = getDirections();
+    for (Direction direction : directions) {
+      allMoves.addAll(getMovesInOneDir(direction));
+    }
+    return Collections.unmodifiableSet(allMoves);
+  }
+
+  /**
+   * Get the moves in one direction. Which is specified by the increment for row
+   * and column
+   *
+   * @param direction The direction to move along
+   */
+  private ArrayList<Move> getMovesInOneDir(Direction direction) {
+    ArrayList<Move> moves = new ArrayList<>();
+    Location current = getLocation();
+    Location increment = current.getIncrement(direction);
+    ChessBoardBase chessBoard = getChessBoard();
+    while (chessBoard.checkIsEmpty(increment)) {
+      Move move = new Move(current, increment);
+      move.setIsAttack(true);
+      moves.add(move);
+      increment = increment.getIncrement(direction);
+    }
+    return moves;
+  }
+
+}
