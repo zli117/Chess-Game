2345e15f5824cfbec742035f8e7b254426a8ac6d
Zonlgin-Li6565 <development.my6565@gmail.com>
2019-01-29 23:42:04 -0600

    Added Rook

---
+++ src/main/java/model/Rook.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import utils.Direction;
+
+public class Rook extends UnboundedPiece {
+
+  public Rook(ChessBoardBase chessBoardBase, Side side) {
+    super(chessBoardBase, side);
+  }
+
+  @Override
+  protected ArrayList<Direction> getDirections() {
+    Direction[] directions = {
+        new Direction(1, 0),
+        new Direction(-1, 0),
+        new Direction(0, 1),
+        new Direction(0, -1)};
+    return new ArrayList<>(Arrays.asList(directions));
+  }
+}
