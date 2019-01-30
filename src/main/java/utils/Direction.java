823240933955c0cc2241f36036bfdda1389abf08
Zonlgin-Li6565 <development.my6565@gmail.com>
2019-01-29 23:38:23 -0600

    Added direction abstraction

---
+++ src/main/java/utils/Direction.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class Direction extends Pair<Integer, Integer> {
+
+  public Direction(int vertical, int horizontal) {
+    super(vertical, horizontal);
+  }
+
+  public int getVerticalDelta() {
+    return getA();
+  }
+
+  public int getHorizontalDelta() {
+    return getB();
+  }
+}
