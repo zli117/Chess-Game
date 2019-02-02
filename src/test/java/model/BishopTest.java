ddf290ef9ac6ea0759dc4d248d91d161afaf803e
Zonlgin-Li6565 <development.my6565@gmail.com>
2019-02-02 12:22:38 -0600

    Fixing test style issues

--- src/test/java/model/BishopTest.java
+++ src/test/java/model/BishopTest.java
@@ -1,6 +1,8 @@
 package model;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Set;
 import org.junit.Test;
