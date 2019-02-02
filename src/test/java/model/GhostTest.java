706b78a6e61c42cb05eb6985788c71981102d82e
Zonlgin-Li6565 <development.my6565@gmail.com>
2019-02-01 19:14:59 -0600

    Added test for ChessBoardBase

---
+++ src/test/java/model/GhostTest.java
@@ -0,0 +1,20 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GhostTest {
+
+  @Test
+  public void killed() {
+  }
+
+  @Test
+  public void isGhost() {
+  }
+
+  @Test
+  public void getMovesAndAttacks() {
+  }
+}
