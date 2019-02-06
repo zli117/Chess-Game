77e7e29abd4fd0c037d7a26d88ea76695f889527
Zonlgin-Li6565 <development.my6565@gmail.com>
2019-02-06 02:46:02 -0600

    Removed unused code

--- src/test/java/utils/VectorTest.java
+++ src/test/java/utils/VectorTest.java
@@ -45,7 +45,53 @@ public class VectorTest {
   }
 
   @RunWith(Parameterized.class)
-  public static class TestSameDirection {
+  public static class TestParallel {
+
+    @Parameter
+    public int mVertical;
+    @Parameter(1)
+    public int mHorizontal;
+    @Parameter(2)
+    public int mSameVertical;
+    @Parameter(3)
+    public int mSameHorizontal;
+    @Parameter(4)
+    public int mDifferentVertical;
+    @Parameter(5)
+    public int mDifferentHorizontal;
+
+
+    /**
+     * Parameters for the test.
+     */
+    @Parameters
+    public static Collection<Object[]> data() {
+      return Arrays.asList(new Object[][]{
+          {0, 0, 0, 0, 1, 1}, {1, 1, 2, 2, 2, 3}, {1, 1, -1, -1, 1, -1},
+          {0, 1, 0, 2, 1, 1}, {-10, 10, 1, -1, 20, 20}
+      });
+    }
+
+    @Test
+    public void testCheckParallel() {
+      Vector vector1 = new Vector(mVertical, mHorizontal);
+      Vector vector2 = new Vector(mSameVertical, mSameHorizontal);
+      Vector vector3 = new Vector(mDifferentVertical, mDifferentHorizontal);
+
+      assertTrue(vector1.checkParallel(vector1));
+      assertTrue(vector2.checkParallel(vector2));
+      assertTrue(vector3.checkParallel(vector3));
+      assertTrue(vector1.checkParallel(vector2));
+      assertTrue(vector2.checkParallel(vector1));
+      assertFalse(vector1.checkParallel(vector3));
+      assertFalse(vector3.checkParallel(vector1));
+      assertFalse(vector3.checkParallel(vector2));
+    }
+
+  }
+
+  @RunWith(Parameterized.class)
+  public static class TestNormalized {
 
     @Parameter
     public int mVertical;
