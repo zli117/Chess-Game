4ffa40ca07a167e367c6d8d0756cd56f961e7bbd
Zonlgin-Li6565 <development.my6565@gmail.com>
2019-02-12 21:23:59 -0600

    Updating Jumper documentation

--- src/main/java/model/Jumper.java
+++ src/main/java/model/Jumper.java
@@ -26,10 +26,10 @@ import utils.Vector;
  * </td> <td> J </td> <td> x </td>
  * </tr>
  * </table>
- * o is reachable location. x is not reachable. It's also a ghost, meaning that
- * normal pieces won't see it and can go pass it. It's also possible that other
- * piece will kick it off the board if those piece happen to be in the same spot
- * as the ghost.
+ * o is reachable location. x is not reachable. The Jumper is also a ghost,
+ * meaning that normal pieces won't see it and can go pass it. It's also
+ * possible that other piece will kick the Jumper off the board if those piece
+ * happen to be in the same spot.
  */
 public class Jumper extends Piece {
 
